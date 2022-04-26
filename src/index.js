de19f33ba803a64bfe8276c7c342bd869da2ada0
SeokYoung <willy4202@naver.com>
2022-04-26 13:03:34 +0900

    Add: 초기세팅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
-import './Styles/reset.scss';
-import './Styles/common.scss';
+import './styles/reset.scss';
+import './styles/common.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Router />);
