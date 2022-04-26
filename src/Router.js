26ee4aa195912073ff183f32d7221fd18c1c3ef6
전형준 <wehj0105@gamil.com>
2022-04-26 21:21:13 +0900

    add: before pull push -v2

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
-import Cart from './pages/Cart/Cart';
 
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Cart />} />;
+        <Route path="/" element={<Main />} />;
       </Routes>
       <Footer />
     </BrowserRouter>
