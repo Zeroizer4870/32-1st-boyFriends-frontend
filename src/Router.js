5de0b1008df6e8cb9e4540952769e020ec159628
SeokYoung <willy4202@naver.com>
2022-04-26 11:27:43 +0900

    modify: 파일 이름 일괄 변경

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './Pages/Main/Main';
-import Nav from './Components/Nav/Nav';
-import Footer from './Components/Footer/Footer';
+import Main from './pages/Main/Main';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer1</div>;
+  return <div>Footer</div>;
 }
 
 export default Footer;
