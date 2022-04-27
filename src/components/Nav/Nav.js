2a25fb385a51131b973826ccf34a9e4c0a1d45b9
Hoon Gi Ahn <ahg4870@gmail.com>
2022-04-27 18:29:20 +0900

    fix:수정완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,80 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Menu from './Menu';
 import './Nav.scss';
 
 function Nav() {
+  const menuList = [
+    {
+      id: 1,
+      menuName: '토이',
+      menuLink: '/toys',
+      subMenu1: '중대형 토이',
+      subMenu1Link: '/toys/bigtoys',
+      subMenu2: '미니 토이',
+      subMenu2Link: '/toys/smalltoys',
+    },
+    {
+      id: 2,
+      menuName: '옷',
+      menuLink: '/cloths',
+      subMenu1: '반팔 의류',
+      subMenu1Link: '/cloths/shortsleeves',
+      subMenu2: '긴팔 의류',
+      subMenu2Link: '/cloths/longsleeves',
+    },
+    {
+      id: 3,
+      menuName: '디지털',
+      menuLink: '/digital',
+      subMenu1: '폰 케이스',
+      subMenu1Link: '/digital/phonecases',
+      subMenu2: '소형 가전',
+      subMenu2Link: '/digital/electronics',
+    },
+  ];
   return (
-    <nav id="nav">
-      <div className="upperWrapper">
-        <div id="upper">
-          <div className="upperLeft">
-            <Link to="#">BOYVER</Link>
-            <Link to="#">보이쇼핑</Link>
+    <nav className="nav">
+      <div className="infoWrapper">
+        <div className="info">
+          <div className="infoLogo">
+            <Link to="/">BoyFriends</Link>
           </div>
-          <div className="upperRight">
-            <Link to="./user/signin">로그인</Link>
-            <Link to="./user/signup">회원가입</Link>
-            <Link to="./cart">장바구니</Link>
+          <div className="infoIcons">
+            <Link to="/user/signin">로그인</Link>
+            <Link to="/user/signup">회원가입</Link>
+            <Link to="/cart">장바구니</Link>
           </div>
         </div>
       </div>
       <div className="logoWrapper">
-        <div id="logo">
-          <div className="logoLeft">
-            <button className="logoLike">좋아요 238,287</button>
+        <div className="logo">
+          <div className="logoLike">
+            <button className="logoLikeBtn">좋아요 238,287</button>
           </div>
           <Link to="/">
-            <img alt="logo" className="logoCenter" src="/images/logo.png" />
+            <img alt="logo" className="logoImg" src="/images/logo.png" />
           </Link>
 
-          <div className="logoRight">
+          <div className="logoSearch">
             <input
               type="text"
-              className="logoRightInput"
+              className="logoSearchInput"
               placeholder="검색어를 입력해보세요"
             />
-            <button className="logoRightInputSearch">
+            <button className="logoSearchInputIcon">
               <i className="fa-solid fa-magnifying-glass" />
             </button>
           </div>
         </div>
       </div>
       <div className="menuWrapper">
-        <div id="menu">
+        <div className="menu">
           <ul className="mainMenu">
-            <li>
-              <Link to="./toy">토이</Link>
-              <ul className="subMenu">
-                <li>
-                  <Link to="./toy/bigtoy">중대형 인형</Link>
-                </li>
-                <li>
-                  <Link to="./toy/smalltoy">미니 인형</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="./cloth">옷</Link>
-              <ul className="subMenu">
-                <li>
-                  <Link to="./cloth/shortsleeve">반팔 의류</Link>
-                </li>
-                <li>
-                  <Link to="./cloth/longsleeve">긴팔 의류</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="./digital">디지털</Link>
-              <ul className="subMenu">
-                <li>
-                  <Link to="./digital/phonecase">폰 케이스</Link>
-                </li>
-                <li>
-                  <Link to="./digital/electronic">소형 가전</Link>
-                </li>
-              </ul>
-            </li>
+            {menuList.map(menu => {
+              return <Menu menu={menu} key={menu.id} />;
+            })}
           </ul>
         </div>
       </div>
