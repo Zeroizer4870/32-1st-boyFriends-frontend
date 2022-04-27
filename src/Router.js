a89016d53fd3d5ee587372762a94386daa64df53
Hoon Gi Ahn <ahg4870@gmail.com>
2022-04-27 10:17:39 +0900

    add:회원가입초기작업

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
+import SignUp from './pages/User/SignUp/Signup';
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />;
+        <Route path="/signup" element={<SignUp />} />;
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/User/SignUp/Signup.js
+++ src/pages/User/SignUp/Signup.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import './Signup.scss';
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div id="signUp">
+      <div className="wrapper">
+        <div className="signUpWrapper">
+          <p className="signUp">회원가입</p>
+        </div>
+        <form className="form">
+          <div className="contentWrapper">
+            <label className="contentLabel">아이디</label>
+            <input
+              type="text"
+              name="id"
+              className="id"
+              placeholder="6자리 이상의 영문 혹은 영문과 숫자를 조합"
+            />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">비밀번호</label>
+            <input
+              type="password"
+              name="pw"
+              className="pw"
+              placeholder="비밀번호를 입력해주세요"
+            />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">비밀번호확인</label>
+            <input
+              type="password"
+              name="pw2"
+              className="pw2"
+              placeholder="비밀번호를 한번 더 입력해주세요"
+            />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">이름</label>
+            <input
+              type="text"
+              name="name"
+              className="name"
+              placeholder="이름을 입력해주세요"
+            />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">휴대폰</label>
+            <input
+              type="number"
+              name="phone"
+              className="phone"
+              placeholder="숫자만 입력해주세요"
+            />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">주소</label>
+            <input
+              type="text"
+              name="address"
+              className="address"
+              placeholder="주소를 입력해주세요"
+            />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">성별</label>
+            <input />
+          </div>
+          <div className="contentWrapper">
+            <label className="contentLabel">생년월일</label>
+            <input
+              type="number"
+              name="birth"
+              className="birth"
+              placeholder=""
+            />
+          </div>
+          <button className="submit">가입하기</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
