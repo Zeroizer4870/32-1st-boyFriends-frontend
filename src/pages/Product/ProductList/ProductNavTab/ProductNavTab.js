ca062dfad3c7dc140e922910ed4310980498bedd
SeokYoung <willy4202@naver.com>
2022-04-27 16:36:55 +0900

    Modify: 링크부분  하드코딩 map 통해 리팩토링

--- src/pages/Product/ProductList/ProductNavTab/ProductNavTab.js
+++ src/pages/Product/ProductList/ProductNavTab/ProductNavTab.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 import '../ProductNavTab/ProductNavTab.scss';
 
 function ProductNavTab({ product }) {
+  const categoryList = ['홈', '토이', `중대형 인형  (${product.length}개)`];
+
+  const spreadLink = categoryList.map((title, index) => {
+    return (
+      <li key={index}>
+        <Link to="/">{title}</Link>
+      </li>
+    );
+  });
+
   return (
     <nav className="categoryWrapper">
       <div className="categoryNav">
         <strong>중대형 인형</strong>
         <div>
-          <ul className="categoryTop">
-            <li>
-              <Link to="/">홈</Link>
-            </li>
-            <li>
-              <Link to="/category">토이</Link>
-            </li>
-            <li>
-              <Link to="/category">중대형 인형 ({product.length}개)</Link>
-            </li>
-          </ul>
+          <ul className="categoryTop">{spreadLink}</ul>
         </div>
       </div>
     </nav>
