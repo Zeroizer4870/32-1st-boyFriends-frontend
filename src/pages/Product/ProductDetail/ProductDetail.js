ea16e4d495308bfe8439f5132c4236fc4bbf1e29
SeokYoung <willy4202@naver.com>
2022-04-28 16:44:04 +0900

    Add: 디테일 페이지 레이아웃 구현 중

--- src/pages/Product/ProductDetail/ProductDetail.js
+++ src/pages/Product/ProductDetail/ProductDetail.js
@@ -5,21 +5,41 @@ function ProductDetail() {
   return (
     <div className="productDetail">
       <div className="layOut">
-        <div className="imgWrapper">
-          <div className="statusTap">NEw</div>
-          <div className="productImg">
+        <div className="productInfoWrapper">
+          <div className="mainProductImg">
+            <span className="statusTap">NEw</span>
             <img
               src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510"
               alt="인형"
             />
           </div>
-          <ul className="imgList">
-            <li>이미지</li>
+          <ul className="imgs">
+            <li className="img">
+              <img src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510" />
+            </li>
+            <li className="img">
+              <img src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510" />
+            </li>
+            <li className="img">
+              <img src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510" />
+            </li>
+            <li className="img">
+              <img src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510" />
+            </li>
+            <li className="img">
+              <img src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510" />
+            </li>
           </ul>
+          <div className="productReviewCount">
+            <div>리뷰수 </div>
+            <div>사용자 총 평점</div>
+          </div>
         </div>
 
         {/* 구분선 */}
-        <div className="productPurchase">ProductPurchase</div>
+        <div className="productPurchase">
+          <div className="productTitle">라인프렌즈 BT CHIMMY 미니쿠션</div>
+        </div>
       </div>
     </div>
   );
