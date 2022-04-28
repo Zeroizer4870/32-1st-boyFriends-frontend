07325758cd141bc99f0c94b1072684cfd6b1806a
SeokYoung <willy4202@naver.com>
2022-04-28 15:38:28 +0900

    Add: 상품 디테일 페이지 레이아웃 구현 중

--- src/pages/Product/ProductDetail/ProductDetail.js
+++ src/pages/Product/ProductDetail/ProductDetail.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import '../ProductDetail/ProductDetail.scss';
 
 function ProductDetail() {
-  return <div>ProductDetail1</div>;
+  return (
+    <div className="productDetail">
+      <div className="layOut">
+        <div className="imgWrapper">
+          <div className="statusTap">NEw</div>
+          <div className="productImg">
+            <img
+              src="https://shop-phinf.pstatic.net/20220426_190/1650958998448KRsls_JPEG/52094832107772977_1629918675.jpg?type=m510"
+              alt="인형"
+            />
+          </div>
+          <ul className="imgList">
+            <li>이미지</li>
+          </ul>
+        </div>
+
+        {/* 구분선 */}
+        <div className="productPurchase">ProductPurchase</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductDetail;
---
+++ src/pages/Product/ProductDetail/ProductPurchase/ProductPurchase.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductPurchase() {
+  return <div className="productPurchase">ProductPurchase</div>;
+}
+
+export default ProductPurchase;
