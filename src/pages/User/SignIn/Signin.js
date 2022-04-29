5d5b6907e2a2fe48625f837eb2d32e14d07d201f
Hoon Gi Ahn <ahg4870@gmail.com>
2022-04-29 11:13:39 +0900

    add:html 틀 구성

--- src/pages/User/SignIn/Signin.js
+++ src/pages/User/SignIn/Signin.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div className="signIn">
+      <div className="contents">
+        <p className="loginText" />
+        <form className="inputWrapper">
+          <input
+            className="email"
+            name="email"
+            type="email"
+            placeholder="아이디를 입력해주세요"
+          />
+          <input
+            className="pw"
+            name="pw"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
+        </form>
+        <div className="findWrapper">
+          <div className="accessCheck" />
+          <div className="findIdPw">
+            <span className="findId" />
+            <span className="findPw" />
+          </div>
+          <div className="buttonWrapper">
+            <button className="loginBtn">로그인</button>
+            <button className="signupBtn">회원가입</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
