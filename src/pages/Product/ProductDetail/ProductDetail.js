8e7e302e6bde2473a3f717afda35a3f4b12c41f8
SeokYoung <willy4202@naver.com>
2022-05-04 10:22:12 +0900

    Add: review 댓글창 추가

--- src/pages/Product/ProductDetail/ProductDetail.js
+++ src/pages/Product/ProductDetail/ProductDetail.js
@@ -3,66 +3,17 @@ import { useParams } from 'react-router-dom';
 import '../ProductDetail/ProductDetail.scss';
 import ProductInfoWrapper from './ProductInfoWrapper/ProductInfoWrapper';
 import ProductPurchase from './ProductPurchase/ProductPurchase';
+import Review from './ProductReview/Review';
 
 function ProductDetail() {
   const [productId, setProductId] = useState(null);
   const params = useParams();
 
-  // const PRODUCT_DATA = {
-  //   id: 1,
-  //   itemTitle: '보이프렌즈 후드티 BT21',
-  //   category: 'toy',
-  //   userName: '성용',
-  //   img: 'https://image.fmkorea.com/files/attach/new/20201223/4477817/2069934950/3279324811/e1f621cd14d2c931f6d5cbf5d38a3b18.png',
-  //   price: 42000,
-  //   productOptioin: [
-  //     // { id: 1, size: 'S', stock: 12, category: '의류' },
-  //     // { id: 2, size: 'M', stock: 0, category: '의류' },
-  //     // { id: 3, size: 'L', stock: 342, category: '의류' },
-  //     { id: 4, size: '단품', stock: 1, category: `toy` },
-  //   ],
-  //   sale: 12,
-  //   status: 'New',
-  //   review: [
-  //     {
-  //       id: 1,
-  //       reviewerName: '기성',
-  //       reviewContent: '너무 좋아요',
-  //       reputation: 5,
-  //       reivewImg:
-  //         'https://file.mk.co.kr/meet/neds/2021/02/image_readtop_2021_26945_16137156464500629.jpg',
-  //     },
-  //     {
-  //       id: 2,
-  //       reviewerName: '성용',
-  //       reviewContent: '너무 좋은데요?',
-  //       reputation: 4,
-  //       reivewImg:
-  //         'https://file.mk.co.kr/meet/neds/2021/02/image_readtop_2021_26945_16137156464500629.jpg',
-  //     },
-  //     {
-  //       id: 3,
-  //       reviewerName: '재관',
-  //       reviewContent: '아름답네요',
-  //       reputation: 2,
-  //       reivewImg:
-  //         'https://file.mk.co.kr/meet/neds/2021/02/image_readtop_2021_26945_16137156464500629.jpg',
-  //     },
-  //     {
-  //       id: 4,
-  //       reviewerName: '훈기',
-  //       reviewContent: '너무 좋아요',
-  //       reputation: 3,
-  //       reivewImg:
-  //         'https://file.mk.co.kr/meet/neds/2021/02/image_readtop_2021_26945_16137156464500629.jpg',
-  //     },
-  //   ],
-  // };
-
   useEffect(() => {
     fetch(`http://10.58.1.227:1234/products/${params.id}`, {
       headers: {
-        Authorization: 'token',
+        Authorization:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZXhwIjoxNjUxNzEwMzQ2fQ.gZh4oqlfy4xJKITrdf3wN99vyZCp5ki172Z3kKAjOWg',
       },
     })
       .then(res => res.json())
@@ -74,12 +25,15 @@ function ProductDetail() {
   return (
     <div>
       {productId && (
-        <div className="productDetail">
-          <div className="layOut">
-            <ProductInfoWrapper productData={productId} />
-            <ProductPurchase productData={productId} />
+        <>
+          <div className="productDetail">
+            <div className="layOut">
+              <ProductInfoWrapper productData={productId} />
+              <ProductPurchase productData={productId} />
+            </div>
           </div>
-        </div>
+          <Review review={productId.review} />
+        </>
       )}
     </div>
   );
